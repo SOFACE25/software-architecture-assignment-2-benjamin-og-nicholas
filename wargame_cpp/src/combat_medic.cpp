2e0cf2a9f313dc75915c10c85c9b60bd75e6186e
BenjaminKromann <benjamin@kromann.info>
2025-02-04 17:03:20 +0100

    now at 100%

--- wargame_cpp/src/combat_medic.cpp
+++ wargame_cpp/src/combat_medic.cpp
@@ -5,5 +5,9 @@ CombatMedic::CombatMedic(int health, int damage) : Soldier(health, damage)
 
 void CombatMedic::heal(Unit &target)
 {
-    target.set_health(target.get_health() + 10);
+    if (target.get_health() > 0) 
+    {
+        target.set_health(target.get_health() + 10);
+    }
+
 }
